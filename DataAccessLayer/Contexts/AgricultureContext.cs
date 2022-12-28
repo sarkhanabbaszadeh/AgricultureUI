2b3496d2a780247a5ff4c3b0b6ba580ee98b65c7
Sarkhan Abbaszadeh <s.abbaszada@ady.az>
2022-12-28 14:49:40 +0400

    add context  and reference

---
+++ DataAccessLayer/Contexts/AgricultureContext.cs
@@ -0,0 +1,32 @@
+﻿
+using EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Contexts
+{
+    public class AgricultureContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=SABBASZADA-N\\MSSQLSERVER2;database=DbAgriculture;integrated security=true");
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Image> Images { get; set; }
+
+        public DbSet<News> Newses { get; set; }
+
+        public DbSet<Service> Services { get; set; }
+
+        public  DbSet<Team> Teams { get; set; }
+
+    }
+}
