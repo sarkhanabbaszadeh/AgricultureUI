4866bfd4a63bd54038d766e6e1b375f42a9af233
Sarkhan Abbaszadeh <s.abbaszada@ady.az>
2023-01-09 10:27:03 +0400

    edit news

---
+++ AgricultureUI/ViewComponents/_NewPartial.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgricultureUI.ViewComponents
+{
+	public class _NewPartial : ViewComponent
+	{
+		private readonly INewsService _newsService;
+
+		public _NewPartial(INewsService newsService)
+		{
+			_newsService = newsService;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			var values=_newsService.GetListAll();
+			return View(values);
+		}
+	}
+}
